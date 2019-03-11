d7209022ff70dfbe86b2a16520a9381d3da5933d
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-03-11 16:26:49 -0300

    Adicionado endpoint para autenticação e entrega de token na API

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,6 +1,48 @@
 from flask import render_template
-from flask import jsonify, request
+from flask import jsonify, request, current_app
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
+
 from app.core import bp
+from app.models import User
+
+#IMPORTANTE, tem que colocar isso header de solicitação
+# headers = {'content-type': 'application/json'} 
+
+# decorator para protger rotas, somente autenticados com token podem ser acessados
+def token_required(f):  
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registeration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            # user = User.query.filter_by(email=data['sub']).first()
+            user = {'id': '1', 'email': 'gomes.fdr@gmail.com', 'password': '1234'}
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
 
 @bp.route('/', defaults={'path': ''})
 @bp.route('/<path:path>')
@@ -8,7 +50,12 @@ def catch_all(path):
     print("Path: {}".format(path))
     return render_template("index.html")
 
+@bp.route('/api/v1/status', methods=['GET'])
+def status():
+    return jsonify({'status': 'ok v1'})
+
 @bp.route('/api/v1/paciente/<cpf>', methods=['GET'])
+@token_required
 def paciente_api(cpf):
     if cpf == '60976900025':
         paciente = {
@@ -42,3 +89,31 @@ def paciente_api(cpf):
             'situacao': 'Não encontrado' 
         }
     return jsonify(paciente), 404
+
+@bp.route('/api/v1/register/', methods=['POST'])
+def register():
+    data = request.get_json()
+    # user = User(**data)
+    # db.session.add(user)
+    # db.session.commit()
+    # return jsonify(user.to_dict()), 201
+    return jsonify(data), 201
+
+@bp.route('/api/v1/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    print('imprimindo data...')
+    print(data)
+    user = User.autorizar(**data)
+
+    if not user:
+        return jsonify({'message': 'Credenciais Invalidas', 'authenticated': False}), 401
+
+    token = jwt.encode({
+        'sub': user['email'],
+        'iat': datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=30)
+    }, '0123456789')
+
+    return jsonify({'token': token.decode('UTF-8')})
+
