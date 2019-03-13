89c362a0c717acfe5186f7d62a88ecd31f390ac3
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-03-13 08:50:44 -0300

    Atualização de dependencias do python

--- run.py
+++ run.py
@@ -18,6 +18,4 @@ def random_number():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    if app.debug:
-        return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
