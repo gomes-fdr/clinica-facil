19fa3ea6ba3e9b6b3d7496757c8d4eff196bac83
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-03-23 14:07:17 -0300

    FIX html dados

--- frontend/src/components/paciente/Dados.vue
+++ frontend/src/components/paciente/Dados.vue
@@ -1,116 +1,72 @@
 <template>
-  <form id="dados">
+  <form>
     <div class="field is-horizontal">
       <div class="field-label is-normal">
         <label class="label">Identificação</label>
       </div>
       <div class="field-body">
-        <b-field
-          :type="{'is-danger': validation.hasError('form.nome') }"
-          :message="[validation.firstError('form.nome')]"
-        >
-          <div class="control is-expanded">
-            <input
-              class="input"
-              id="nome"
-              name="nome"
-              type="text"
-              placeholder="Nome"
-              v-model="form.nome"
-            >
-          </div>
-          <div class="control">
-            <a class="button is-warning" @click.prevent="pesquisarNome">
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input is-danger" type="text" placeholder="Nome">
+            <a class="icon is-small is-right">
               <i class="fas fa-search"></i>
             </a>
-          </div>
-        </b-field>
-
-        <b-field
-          :type="{'is-danger': validation.hasError('form.email') }"
-          :message="[validation.firstError('form.email')]"
-        >
-          <b-input name="email" type="email" placeholder="Email" v-model="form.email"></b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Documentos</label>
-      </div>
-      <div class="field-body">
-        <b-field
-          :type="{'is-danger': validation.hasError('form.dt_nascimento') }"
-          :message="[validation.firstError('form.dt_nascimento')]"
-        >
-          <input
-           name="dt_nascimento"
-           class="input"
-           placeholder="Data Nascimento"
-           v-model="form.dt_nascimento"
-           v-mask="'##/##/####'"
-          >
-        </b-field>
-        <b-field
-          :type="{'is-danger': validation.hasError('form.rg') }"
-          :message="[validation.firstError('form.rg')]"
-        >
-          <input
-           name="rg"
-           class="input"
-           placeholder="Identidade"
-           v-model="form.rg"
-           v-mask="'##########'"
-          >
-        </b-field>
-        <b-field
-          :type="{'is-danger': validation.hasError('form.cpf') }"
-          :message="[validation.firstError('form.cpf')]"
-        >
-          <div class="control is-expanded">
-            <input
-              id="cpf"
-              name="cpf"
-              type="text"
-              class="input"
-              placeholder="CPF"
-              v-model="form.cpf"
-              v-mask="'###.###.###-##'"
-              @keyup.ctrl.70="pesquisarCPF"
-            >
-          </div>
-          <a class="button is-warning" @click.prevent="pesquisarCPF">
-            <i class="fas fa-search"></i>
-          </a>
-        </b-field>
+          </p>
+          <p class="help is-danger">This field is required</p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Email">
+          </p>
+        </div>
       </div>
     </div>
 
     <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Contatos</label>
-      </div>
+      <div class="field-label is-normal"></div>
       <div class="field-body">
-        <b-field
-          :type="{'is-danger': validation.hasError('form.filiacao') }"
-          :message="[validation.firstError('form.filiacao')]"
-        >
-          <b-input name="filiacao" type="text" placeholder="Filiação" v-model="form.filiacao"></b-input>
-        </b-field>
-        <b-field>
-          <b-input name="profissao" type="text" placeholder="Profissão" v-model="form.profissao"></b-input>
-        </b-field>
-        <b-field
-          :type="{'is-danger': validation.hasError('form.responsavel') }"
-          :message="[validation.firstError('form.responsavel')]"
-        >
-          <b-input
-            name="responsavel"
-            placeholder="Responsável"
-            :disabled="isChild"
-            v-model="form.responsavel"
-          ></b-input>
-        </b-field>
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input is-danger" type="text" placeholder="Data de Nascimento">
+          </p>
+          <p class="help is-danger">This field is required</p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input is-danger" type="text" placeholder="CPF">
+            <a class="icon is-small is-right">
+              <i class="fas fa-search"></i>
+            </a>
+          </p>
+          <p class="help is-danger">This field is required</p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input" type="text" placeholder="Identidade">
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label is-normal"></div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Nome da mãe">
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input" type="text" placeholder="Profissão">
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input is-danger" type="text" placeholder="Responsável">
+          </p>
+          <p class="help is-danger">This field is required</p>
+        </div>
       </div>
     </div>
 
@@ -119,126 +75,88 @@
         <label class="label">Telefones</label>
       </div>
       <div class="field-body">
-        <b-field>
-          <input
-           name="celular"
-           type="text"
-           class="input"
-           placeholder="Celular"
-           v-model="form.t_celular"
-           v-mask="'(##)# #### ####'"
-          >
-        </b-field>
-        <b-field>
-          <input
-           name="fixo"
-           class="input"
-           type="text"
-           placeholder="Fixo"
-           v-model="form.t_fixo"
-           v-mask="'(##)#### ####'"
-          >
-        </b-field>
-        <b-field>
-          <input
-            name="tel_resp"
-            class="input"
-            placeholder="Telefone Responsável"
-            :disabled="isChild"
-            v-model="form.t_reponsavel"
-            v-mask="'(##)# #### ####'"
-          >
-        </b-field>
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Telefone Celular">
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input" type="text" placeholder="Telefone Fixo">
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input is-danger" type="text" placeholder="Telefone Responsável">
+          </p>
+          <p class="help is-danger">This field is required</p>
+        </div>
       </div>
     </div>
 
-    <section id="endereco">
-      <div class="field is-horizontal">
-        <div class="field-label is-normal">
-          <label class="label">Endereço</label>
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">Endereço</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Rua">
+          </p>
         </div>
-        <div class="field-body">
-          <b-field
-            :type="{'is-danger': validation.hasError('form.endereco.cep') }"
-            :message="[validation.firstError('form.endereco.cep')]"
-          >
-            <div class="control is-expanded">
-              <input
-                class="input"
-                id="cep"
-                name="cep"
-                type="text"
-                placeholder="CEP"
-                v-model="form.endereco.cep"
-                v-mask="'##.###-###'"
-              >
-            </div>
-            <div class="control">
-              <a class="button is-warning" @click.prevent="pesquisarCEP">
-                <i class="fas fa-search"></i>
-              </a>
-            </div>
-          </b-field>
-          <b-field
-            :type="{'is-danger': validation.hasError('form.endereco.rua') }"
-            :message="[validation.firstError('form.endereco.rua')]"
-          >
-            <b-input name="rua" placeholder="Rua" v-model="form.endereco.rua"></b-input>
-          </b-field>
-          <b-field
-            :type="{'is-danger': validation.hasError('form.endereco.numero') }"
-            :message="[validation.firstError('form.endereco.numero')]"
-          >
-            <b-input name="numero" placeholder="Número" v-model="form.endereco.numero"></b-input>
-          </b-field>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input" type="text" placeholder="Numero">
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input" type="text" placeholder="Complemento">
+          </p>
         </div>
       </div>
+    </div>
 
-      <div class="field is-horizontal">
-        <div class="field-label is-normal">
-          <label class="label">Endereço</label>
+    <div class="field is-horizontal">
+      <div class="field-label is-normal"></div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Cidade">
+          </p>
         </div>
-        <div class="field-body">
-          <b-field>
-            <b-input
-              name="complemento"
-              placeholder="Complemento"
-              v-model="form.endereco.complemento"
-            ></b-input>
-          </b-field>
-          <b-field
-            :type="{'is-danger': validation.hasError('form.endereco.cidade') }"
-            :message="[validation.firstError('form.endereco.cidade')]"
-          >
-            <b-input name="cidade" placeholder="Cidade" v-model="form.endereco.cidade"></b-input>
-          </b-field>
-          <b-field
-            :type="{'is-danger': validation.hasError('form.endereco.estado') }"
-            :message="[validation.firstError('form.endereco.estado')]"
-          >
-            <b-input name="estado" placeholder="Estado" v-model="form.endereco.estado"></b-input>
-          </b-field>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input" type="text" placeholder="Estado">
+          </p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input class="input is-danger" type="text" placeholder="CEP">
+            <a class="icon is-small is-right">
+              <i class="fas fa-search"></i>
+            </a>
+          </p>
+          <p class="help is-danger">This field is required</p>
         </div>
       </div>
-    </section>
-    <br>
-
+    </div>
     <div class="field is-grouped is-grouped-right">
       <div class="control is-grouped-right">
-        <b-switch type="is-info" v-model="this.hasCelPhone">Envio de SMS?</b-switch>
-        <b-switch type="is-info" v-model="form.adultoInapto">Adulto com Responsável?</b-switch>
+        <b-switch type="is-info">Envio de SMS?</b-switch>
+        <b-switch type="is-info">Adulto com Responsável?</b-switch>
       </div>
     </div>
     <hr>
     <div class="field is-grouped is-grouped-right">
       <p class="control">
-        <button type="reset" @click.prevent="reset" class="button">Limpar</button>
+        <button type="reset" class="button">Limpar</button>
       </p>
       <p class="control">
-        <a class="button" :disabled="bNovo">Novo</a>
+        <a class="button">Novo</a>
       </p>
       <p class="control">
-        <button class="button" @click.prevent="submit" :disabled="bAtualizar">Atualizar</button>
+        <button class="button" @click.prevent="submit">Atualizar</button>
       </p>
     </div>
   </form>
@@ -313,7 +231,14 @@ export default {
       return Validator.value(value).required()
     },
     'form.responsavel': function (value) {
-      return Validator.value(value).required()
+      if (this.form.adultoInapto === true) {
+        return Validator.value(value).required()
+      }
+    },
+    'form.t_responsavel': function (value) {
+      if (this.form.adultoInapto === true) {
+        return Validator.value(value).required()
+      }
     },
     'form.endereco.cep': function (value) {
       return Validator.value(value).required()
@@ -334,7 +259,7 @@ export default {
   methods: {
     submit () {
       var vm = this
-      // this.$bus.$emit("submit");
+      // this.$bus.$emit('submit');
       this.$validate().then(function (success) {
         if (success) {
           // console.log('Validou, enviando...');
@@ -370,7 +295,9 @@ export default {
       cpf = cpf.replace('.', '')
       cpf = cpf.replace('-', '')
       this.$http
-        .get(`http://localhost:5000/api/v1/paciente/cpf/${cpf}`, {'headers': {'Authorization': `b: ${auth.getToken()}`}})
+        .get(`http://localhost:5000/api/v1/paciente/cpf/${cpf}`, {
+          headers: { Authorization: `b: ${auth.getToken()}` }
+        })
         .then(function (response) {
           if (response.data) {
             vm.form = response.data
@@ -388,7 +315,9 @@ export default {
               type: 'is-danger',
               position: 'is-bottom'
             })
-            setTimeout(function () { auth.logout() }, 5000)
+            setTimeout(function () {
+              auth.logout()
+            }, 5000)
           } else {
             this.$toast.open({
               duration: 5000,
