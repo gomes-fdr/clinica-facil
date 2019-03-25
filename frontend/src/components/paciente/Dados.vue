ef0b68252a68d436804c3aedfa28e88d535fa833
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-03-25 09:32:04 -0300

    FIX: posição das mensagens de erro

--- frontend/src/components/paciente/Dados.vue
+++ frontend/src/components/paciente/Dados.vue
@@ -6,18 +6,35 @@
       </div>
       <div class="field-body">
         <div class="field">
-          <p class="control is-expanded has-icons-right">
-            <input class="input is-danger" type="text" placeholder="Nome">
-            <a class="icon is-small is-right">
-              <i class="fas fa-search"></i>
-            </a>
+        <div class="field has-addons">
+          <p class="control is-expanded">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.nome') }"
+             type="text"
+             placeholder="Nome"
+             v-model="form.nome"
+            >
           </p>
-          <p class="help is-danger">This field is required</p>
+            <div class="control">
+              <a class="button is-right" @click.prevent="pesquisarNome">
+                <i class="fas fa-search"></i>
+              </a>
+            </div>
+        </div>
+          <p v-show="validation.hasError('form.nome')" class="help is-danger">{{ validation.firstError('form.nome') }}</p>
         </div>
         <div class="field">
           <p class="control is-expanded">
-            <input class="input" type="text" placeholder="Email">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.email') }"
+             type="text"
+             placeholder="Email"
+             v-model="form.email"
+            >
           </p>
+          <p v-show="validation.hasError('form.email')" class="help is-danger">{{ validation.firstError('form.email') }}</p>
         </div>
       </div>
     </div>
@@ -27,23 +44,49 @@
       <div class="field-body">
         <div class="field">
           <p class="control is-expanded">
-            <input class="input is-danger" type="text" placeholder="Data de Nascimento">
+            <input 
+             class="input"
+             :class="{'is-danger': validation.hasError('form.dt_nascimento') }"
+             type="text"
+             placeholder="Data de Nascimento"
+             v-model="form.dt_nascimento"
+             v-mask="'##/##/####'"
+            >
           </p>
-          <p class="help is-danger">This field is required</p>
+          <p v-show="validation.hasError('form.dt_nascimento')" class="help is-danger">{{ validation.firstError('form.dt_nascimento') }}</p>
         </div>
         <div class="field">
+        <div class="field has-addons">
           <p class="control is-expanded has-icons-right">
-            <input class="input is-danger" type="text" placeholder="CPF">
-            <a class="icon is-small is-right">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.cpf') }"
+             type="text"
+             placeholder="CPF"
+             v-model="form.cpf"
+             v-mask="'###########'"
+            >
+          </p>
+          <div class="control">
+            <a class="button  is-right" @click.prevent="pesquisarCPF">
               <i class="fas fa-search"></i>
             </a>
-          </p>
-          <p class="help is-danger">This field is required</p>
+          </div>
+        </div>
+          <p v-show="validation.hasError('form.cpf')" class="help is-danger">{{ validation.firstError('form.cpf') }}</p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
-            <input class="input" type="text" placeholder="Identidade">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.rg') }"
+             type="text"
+             placeholder="Identidade"
+             v-model="form.rg"
+             v-mask="'##########'"
+            >
           </p>
+          <p v-show="validation.hasError('form.rg')" class="help is-danger">{{ validation.firstError('form.rg') }}</p>
         </div>
       </div>
     </div>
@@ -53,19 +96,37 @@
       <div class="field-body">
         <div class="field">
           <p class="control is-expanded">
-            <input class="input" type="text" placeholder="Nome da mãe">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.filiacao')}"
+             type="text"
+             placeholder="Filiação"
+             v-model="form.filiacao"
+            >
+          </p>
+          <p v-show="validation.hasError('form.filiacao') " class="help is-danger">{{ validation.firstError('form.filiacao') }}</p>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-right">
+            <input
+             class="input"
+             type="text"
+             placeholder="Profissão"
+             v-model="form.profissao"
+            >
           </p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
-            <input class="input" type="text" placeholder="Profissão">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.responsavel')}"
+             type="text"
+             placeholder="Responsável"
+             v-model="form.responsavel"
+            >
           </p>
-        </div>
-        <div class="field">
-          <p class="control is-expanded has-icons-right">
-            <input class="input is-danger" type="text" placeholder="Responsável">
-          </p>
-          <p class="help is-danger">This field is required</p>
+          <p v-show="validation.hasError('form.responsavel') " class="help is-danger">{{ validation.firstError('form.responsavel') }}</p>
         </div>
       </div>
     </div>
@@ -77,19 +138,38 @@
       <div class="field-body">
         <div class="field">
           <p class="control is-expanded">
-            <input class="input" type="text" placeholder="Telefone Celular">
+            <input
+             class="input"
+             type="text"
+             placeholder="Telefone Celular"
+             v-model="form.t_celular"
+             v-mask="'###########'"
+            >
           </p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
-            <input class="input" type="text" placeholder="Telefone Fixo">
+            <input
+             class="input"
+             type="text"
+             placeholder="Telefone Fixo"
+             v-model="form.t_fixo"
+             v-mask="'##########'"
+            >
           </p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
-            <input class="input is-danger" type="text" placeholder="Telefone Responsável">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.t_responsavel')}"
+             type="text"
+             placeholder="Telefone Responsável"
+             v-model="form.t_responsavel"
+             v-mask="'###########'"
+            >
           </p>
-          <p class="help is-danger">This field is required</p>
+          <p v-show="validation.hasError('form.t_responsavel') " class="help is-danger">{{ validation.firstError('form.t_responsavel') }}</p>
         </div>
       </div>
     </div>
@@ -101,13 +181,27 @@
       <div class="field-body">
         <div class="field">
           <p class="control is-expanded">
-            <input class="input" type="text" placeholder="Rua">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.endereco.rua')}"
+             type="text"
+             placeholder="Rua"
+             v-model="form.rua"
+            >
           </p>
+          <p v-show="validation.hasError('form.endereco.rua') " class="help is-danger">{{ validation.firstError('form.endereco.rua') }}</p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
-            <input class="input" type="text" placeholder="Numero">
+            <input
+             class="input"
+             :class="{'is-danger': validation.hasError('form.endereco.numero')}"
+             type="text"
+             placeholder="Numero"
+             v-model="form.endereco.numero"
+            >
           </p>
+          <p v-show="validation.hasError('form.endereco.numero') " class="help is-danger">{{ validation.firstError('form.endereco.numero') }}</p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
@@ -122,35 +216,58 @@
       <div class="field-body">
         <div class="field">
           <p class="control is-expanded">
-            <input class="input" type="text" placeholder="Cidade">
+            <input 
+            class="input"
+            :class="{'is-danger': validation.hasError('form.endereco.cidade')}"
+            type="text"
+            placeholder="Cidade"
+            v-model="form.endereco.cidade"
+            >
           </p>
+          <p v-show="validation.hasError('form.endereco.cidade') " class="help is-danger">{{ validation.firstError('form.endereco.cidade') }}</p>
         </div>
         <div class="field">
           <p class="control is-expanded has-icons-right">
-            <input class="input" type="text" placeholder="Estado">
+            <input 
+             class="input"
+             :class="{'is-danger': validation.hasError('form.endereco.estado')}"
+             type="text"
+             placeholder="Estado"
+             v-model="form.endereco.estado"
+            >
           </p>
+          <p v-show="validation.hasError('form.endereco.estado') " class="help is-danger">{{ validation.firstError('form.endereco.estado') }}</p>
         </div>
         <div class="field">
+        <div class="field has-addons">
           <p class="control is-expanded has-icons-right">
-            <input class="input is-danger" type="text" placeholder="CEP">
-            <a class="icon is-small is-right">
+            <input class="input"
+             type="text"
+              placeholder="CEP"
+              v-model="form.endereco.cep"
+              v-mask="'########'"
+            >
+          </p>
+          <div class="control">
+            <a class="button  is-right" @click.prevent="pesquisarCEP">
               <i class="fas fa-search"></i>
             </a>
-          </p>
-          <p class="help is-danger">This field is required</p>
+          </div>
+        </div>
         </div>
       </div>
     </div>
+
     <div class="field is-grouped is-grouped-right">
       <div class="control is-grouped-right">
         <b-switch type="is-info">Envio de SMS?</b-switch>
-        <b-switch type="is-info">Adulto com Responsável?</b-switch>
+        <b-switch type="is-info" v-model="form.adultoInapto">Adulto com Responsável?</b-switch>
       </div>
     </div>
     <hr>
     <div class="field is-grouped is-grouped-right">
       <p class="control">
-        <button type="reset" class="button">Limpar</button>
+        <button type="reset" class="button" @click.prevent="reset">Limpar</button>
       </p>
       <p class="control">
         <a class="button">Novo</a>
@@ -191,7 +308,7 @@ export default {
         responsavel: '',
         t_celular: '',
         t_fixo: '',
-        t_reponsavel: '',
+        t_responsavel: '',
         endereco: {
           cep: '',
           rua: '',
@@ -225,20 +342,18 @@ export default {
     'form.cpf': function (value) {
       return Validator.value(value)
         .required()
-        .length(11 + 3)
+        .length(11)
     },
     'form.filiacao': function (value) {
       return Validator.value(value).required()
     },
     'form.responsavel': function (value) {
-      if (this.form.adultoInapto === true) {
+      if (this.form.adultoInapto) {
         return Validator.value(value).required()
       }
     },
     'form.t_responsavel': function (value) {
-      if (this.form.adultoInapto === true) {
-        return Validator.value(value).required()
-      }
+      return Validator.value(value).required()
     },
     'form.endereco.cep': function (value) {
       return Validator.value(value).required()
@@ -388,7 +503,7 @@ export default {
       this.form.endereco.cidade = ''
       this.form.endereco.estado = ''
       this.form.envio_sms = ''
-      this.form.adultoInapto = ''
+      this.form.adultoInapto = false
       this.isFetching = false
       this.data = []
       this.bNovo = true
