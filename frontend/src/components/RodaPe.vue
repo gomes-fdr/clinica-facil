edc0c7f223bfa5689b641da03f265e14bdcd8b61
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-05-03 11:28:16 -0300

    Versão para primeira release

--- frontend/src/components/RodaPe.vue
+++ frontend/src/components/RodaPe.vue
@@ -2,7 +2,7 @@
   <footer class="hero-foot has-text-dark">
     <div class="content has-text-centered">
       <p>
-        <strong>Clinica Fácil - v-0.1.0</strong> por
+        <strong>Clinica Fácil - v-1.0.0</strong> por
         <a href="https://gomes-fdr.github.io">Fargos Sistemas</a>
       </p>
     </div>
