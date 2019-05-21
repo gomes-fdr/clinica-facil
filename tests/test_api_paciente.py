83dfee9aeca34aecd9c0271926effafe0da68517
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2019-05-21 12:56:51 -0300

    Add testes para pacientes

---
+++ tests/test_api_paciente.py
@@ -0,0 +1,16 @@
+from flask import url_for
+from tests.base_test import TestFlaskBase
+
+class TestPaciente(TestFlaskBase):
+    """
+    Rotas para manipulação de Pacientes
+    """
+
+    def test_pega_os_pacientes_cadastrados(self):
+        # TODO: TESTE
+        ...
+
+    def test_atualiza_um_paciente(self):
+        # TODO: TESTE
+        ...
+
